0077387376349cf7f87b51c9617c0965a9b71e2e
Alexlmagno <alejandro.amado.piano@gmail.com>
2018-09-06 15:14:20 +0200

    Elegido semantic-ui-react como Front-end framework. Añadido el Layout de Homepage

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import '../node_modules/semantic-ui-css/semantic.min.css';
+import HomepageLayout from './HomepageLayout';
+
+ReactDOM.render(<HomepageLayout />, document.getElementById('root'));
 registerServiceWorker();
